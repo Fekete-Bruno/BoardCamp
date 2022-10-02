9ae2ac2007dfd062166521811e0f076146f0062f
Fekete-Bruno <brunohffekete@gmail.com>
2022-10-01 22:24:24 -0300

    feat: GET games query working

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -1,13 +1,20 @@
 import connection from "../database/database.js";
 
 async function getGames(req,res){
+    const nameQuery = req.query.name?.toLowerCase();
+    let query = []
     try {
-        const query = await connection.query('SELECT * FROM games');
-        return res.send(query.rows);
+        query = await connection.query('SELECT games.*, categories.name as "categorieName" FROM games JOIN categories ON games."categoryId" = categories.id;');
     } catch (error) {
         console.error(error);
         return res.sendStatus(500);
     }
+
+    if(nameQuery){
+        return res.send(query.rows.filter((el)=>el.name.toLowerCase().includes(nameQuery)));
+    }
+
+    return res.send(query.rows);
 }
 
 export { getGames };
